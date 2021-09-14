5b929415a2a60d59fa5099eda81512a9a206897f
Grace <grace.nduta@mawingunetworks.com>
2021-09-14 10:51:41 +0300

    added tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,11 @@
-describe('CRUD Stickers', () => {
-    
-})
+const knex = require("../db/knex");
+
+describe("CRUD Stickers", () => {
+  before(() => {
+    // run migrations
+    knex.migrate.latest().then(() => {
+      // run seeds
+      return knex.seed.run();
+    });
+  });
+});
